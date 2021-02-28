55d181644c961c50000b616322c2860e77e87028
PawelSroka <pawel.h.sroka@gmail.com>
2021-02-28 15:39:44 +0100

    first commit

---
+++ pl.podroznik/src/main/java/project/Main.java
@@ -0,0 +1,36 @@
+package main.java.project;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+    // Application starts
+
+    // pakiety:
+    // 1. controller
+    // 2. service
+    // 3. dao (data access object) <- baza danych
+    // 4. model
+
+    // Model (domain):
+    // Wszystkie encje, wszystkie klasy, które są w jakis sposób zapisyuwane w bazie danych
+
+    // Controller <- mapowanie "raw" danych na klasy
+    // wejscie do aplikacji (interfejs konsolowy)
+    // wc zytaj od uzytkownika
+    // wypisz do uzytkownika
+    // pokaz menu itd.
+
+    // service (logika biznesowa)
+    // operacje na bazach danych
+    // obsluga sytuacji biznesowo nieprzewidzianych
+
+    // dao
+    // dostep do bazy danych
+    // zapisywanie do bazy danych
+    // wczytywanie z bazy danych
+    // sprawdzanie czy istnieje w bazie
+    // cała komunikacja z bazą danych
+
+}
