74cb9c1b3e0bd21b85a1d707ab12734238eafe27
PawelSroka <pawel.h.sroka@gmail.com>
2021-03-06 17:59:34 +0100

    Refactoring function no. 3

--- src/main/java/project/service/ShowNodeList.java
+++ src/main/java/project/service/ShowNodeList.java
@@ -8,16 +8,21 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.TypedQuery;
 import java.util.List;
+import java.util.Scanner;
 
 public class ShowNodeList {
 
     public static void showNodeList(EntityManager manager){
+        Scanner sc =new Scanner(System.in);
 
     TypedQuery<CommunicationNode> fromCommunicationNode = manager.createQuery("from CommunicationNode", CommunicationNode.class);
     List<CommunicationNode> fromNodeList = fromCommunicationNode.getResultList();
+        System.out.println();
     for (CommunicationNode node : fromNodeList){
         System.out.println("Nazwa dworca: " + node.getNodeName() + "\nMiasto: " + node.getNodeCity() + "\nulica: " + node.getNodeAdress());
+        System.out.println();
     }
-
+        System.out.println("type enter to continue...");
+    sc.nextLine();
 }
 }
